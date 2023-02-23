8c4e098cc0ab8f4ba350dc0b730549eb1c7c727f
Nainil30 <maladkar.n@northeastern.edu>
2023-02-23 16:38:22 -0500

    final assignment

--- terraform/module/networking/variables.tf
+++ terraform/module/networking/variables.tf
@@ -26,8 +26,8 @@ variable "cidr" {
   # default     = "10.0.0.0/16"
 
   #validation {
-   # condition     = can(regex("^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])(\\/(|1[6-9]|2[0-8]))$", var.cidr))
-    #error_message = "CIDR block parameter must be of the format x.x.x.x/16-28."
+  # condition     = can(regex("^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])(\\/(|1[6-9]|2[0-8]))$", var.cidr))
+  #error_message = "CIDR block parameter must be of the format x.x.x.x/16-28."
   #}
 }
 
