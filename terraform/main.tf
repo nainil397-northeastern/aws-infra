c4571575138102674aec812ed2e8a7e552b13338
Nainil30 <maladkar.n@northeastern.edu>
2023-02-23 07:29:39 -0500

    changes to terraform file for assignment 4

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,16 +1,25 @@
-# locals {
-#   vpc_cidrs = [for number in range(0, "${var.vpc_count}") : cidrsubnet("${var.cidr}", 8, number)]
-# }
+locals {
+  vpc_cidrs = [for number in range(0, var.vpc_count) : cidrsubnet(var.cidr, 8, number)]
+}
 
-module "myNetwork" {
-  count  = var.vpc_count
+module "mynetwork" {
   source = "./module/networking"
-  # cidr   = local.vpc_cidrs[count.index]
-  cidr = var.cidr
 
-  priv_sub_count = var.priv_sub_count
-  pub_sub_count  = var.pub_sub_count
-  region         = var.region
-  environment    = var.environment
-
+  region                  = var.region
+  environment             = var.environment
+  vpc_count               = var.vpc_count
+  count                   = var.vpc_count
+  cidr                    = local.vpc_cidrs[count.index]
+  priv_sub_count          = var.priv_sub_count
+  pub_sub_count           = var.pub_sub_count
+  destination_cidr        = var.destination_cidr
+  ingress_cidr            = var.ingress_cidr
+  public_key_loc          = var.public_key_loc
+  my_ami_id               = var.my_ami_id
+  instance_type           = var.instance_type
+  delete_on_termination   = var.delete_on_termination
+  volume_size             = var.volume_size
+  volume_type             = var.volume_type
+  disable_api_termination = var.disable_api_termination
+  app_port                = var.app_port
 }
--- terraform/module/networking/network.tf
+++ terraform/module/networking/network.tf
@@ -9,6 +9,9 @@ resource "aws_vpc" "nainil" {
   tags = {
     Name = "Main VPC"
   }
+
+  enable_dns_hostnames = true
+  enable_dns_support   = true
 }
 
 resource "aws_subnet" "nainil_private_subnet" {
@@ -22,7 +25,7 @@ resource "aws_subnet" "nainil_private_subnet" {
 
   tags = {
     Name        = "Private Subnet ${count.index + 1}"
-    Environment = "${var.environment}"
+    Environment = var.environment
   }
 
   depends_on = [
@@ -35,13 +38,13 @@ resource "aws_subnet" "nainil_public_subnet" {
 
   count             = var.pub_sub_count
   cidr_block        = element(local.public_subnet_cidrs, count.index)
-  availability_zone = data.aws_availability_zones.azs.names[count.index]
+  availability_zone = data.aws_availability_zones.azs.names[count.index % length(data.aws_availability_zones.azs.names)]
 
   map_public_ip_on_launch = true
 
   tags = {
     Name        = "Public Subnet ${count.index + 1}"
-    Environment = "${var.environment}"
+    Environment = var.environment
   }
 
   depends_on = [
@@ -75,7 +78,7 @@ resource "aws_route_table" "nainil_public_route_table" {
 
 resource "aws_route" "nainil_public_route" {
   route_table_id         = aws_route_table.nainil_public_route_table.id
-  destination_cidr_block = "0.0.0.0/0"
+  destination_cidr_block = var.destination_cidr
   gateway_id             = aws_internet_gateway.nainil_internet_gateway.id
 
   depends_on = [
@@ -116,3 +119,89 @@ resource "aws_route_table_association" "nainil_priv_rt_association" {
   ]
 }
 
+resource "aws_security_group" "application" {
+  name   = "application"
+  vpc_id = aws_vpc.nainil.id
+
+  #Incoming traffic
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.ingress_cidr]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = [var.ingress_cidr]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = [var.ingress_cidr]
+  }
+
+  ingress {
+    from_port   = var.app_port
+    to_port     = var.app_port
+    protocol    = "tcp"
+    cidr_blocks = [var.ingress_cidr]
+  }
+
+  #Outgoing traffic
+  egress {
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
+    cidr_blocks = [var.ingress_cidr]
+  }
+  tags = {
+    Name = "application"
+  }
+
+  depends_on = [
+    aws_vpc.nainil
+  ]
+}
+
+resource "aws_key_pair" "nainil_ec2_key" {
+  key_name   = "nainil_ec2_key"
+  public_key = file(var.public_key_loc)
+
+  depends_on = [
+    aws_vpc.nainil
+  ]
+}
+
+resource "aws_instance" "nainil_aws" {
+  ami                         = var.my_ami_id
+  instance_type               = var.instance_type
+  key_name                    = aws_key_pair.nainil_ec2_key.key_name
+  subnet_id                   = aws_subnet.nainil_public_subnet[0].id
+  associate_public_ip_address = true
+
+  #   security_groups = ["application_security_group"]
+  vpc_security_group_ids = [aws_security_group.application.id]
+
+  disable_api_termination = var.disable_api_termination
+  root_block_device {
+    volume_size           = var.volume_size
+    volume_type           = var.volume_type
+    delete_on_termination = var.delete_on_termination
+  }
+
+  tags = {
+    Name = "EC2 Instance ${timestamp()}"
+  }
+
+  depends_on = [
+    aws_subnet.nainil_public_subnet,
+    aws_security_group.application
+  ]
+
+
+}
