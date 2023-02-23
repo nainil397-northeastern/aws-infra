194f07e5807f1b1112c53a61e8e577739082476c
Nainil30 <maladkar.n@northeastern.edu>
2023-02-23 16:35:06 -0500

    final iteration

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -25,10 +25,10 @@ variable "cidr" {
   description = "CIDR block for the VPC"
   # default     = "10.0.0.0/16"
 
-  validation {
-    condition     = can(regex("^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])(\\/(|1[6-9]|2[0-8]))$", var.cidr))
-    error_message = "CIDR block parameter must be of the format x.x.x.x/16-28."
-  }
+  #validation {
+  #  condition     = can(regex("^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])(\\/(|1[6-9]|2[0-8]))$", var.cidr))
+  #  error_message = "CIDR block parameter must be of the format x.x.x.x/16-28."
+  #}
 }
 
 variable "vpc_count" {
