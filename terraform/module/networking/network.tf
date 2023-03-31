76de806bbf5387ae489878da77232a743a2a982d
Nainil30 <maladkar.n@northeastern.edu>
2023-03-31 01:41:02 -0400

    changes to network file

--- terraform/module/networking/network.tf
+++ terraform/module/networking/network.tf
@@ -410,11 +410,13 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "mySecurity" {
 resource "aws_iam_role" "EC2-CSYE6225" {
   name                = "EC2-CSYE6225"
   assume_role_policy  = data.aws_iam_policy_document.instance_assume_role_policy.json
-  managed_policy_arns = [aws_iam_policy.WebAppS3.arn]
+  managed_policy_arns = [aws_iam_policy.WebAppS3.arn, aws_iam_policy.cloudwatch_agent_policy.arn]
 
   depends_on = [
     data.aws_iam_policy_document.instance_assume_role_policy,
-    aws_iam_policy.WebAppS3
+    aws_iam_policy.WebAppS3,
+    aws_iam_policy.cloudwatch_agent_policy
+
   ]
 }
 
